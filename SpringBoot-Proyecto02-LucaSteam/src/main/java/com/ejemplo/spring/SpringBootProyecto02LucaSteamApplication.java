3b99c89912534142ad2533ef71582007f50680f9
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-11-21 13:47:07 +0100

    Carpeta del pryecto inicial y product backlog

---
+++ SpringBoot-Proyecto02-LucaSteam/src/main/java/com/ejemplo/spring/SpringBootProyecto02LucaSteamApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProyecto02LucaSteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProyecto02LucaSteamApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Proyecto02-LucaSteam/src/test/java/com/ejemplo/spring/SpringBootProyecto02LucaSteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProyecto02LucaSteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
