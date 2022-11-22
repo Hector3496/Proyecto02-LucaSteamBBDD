45ceb47c89ed4f32553e92db133e869b331152b6
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-11-22 10:34:20 +0100

    Carpeta sql

--- SpringBoot-Proyecto02-LucaSteam/sql/MySQL_Script.sql
+++ SpringBoot-Proyecto02-LucaSteam/sql/MySQL_Script.sql
@@ -3,9 +3,11 @@ use videojuegosdb;
 
 CREATE TABLE `videojuegos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `year` int(45) NOT NULL,
+  `genre` varchar(45) NOT NULL,
+  `platform` varchar(45) NOT NULL,
+  `publisher` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1
 
