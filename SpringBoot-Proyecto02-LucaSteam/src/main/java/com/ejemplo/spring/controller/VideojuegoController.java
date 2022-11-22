4508c63d0ef6383220eb8280be84778c54d8484c
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-11-22 10:39:06 +0100

    Modificadas clases service y repository

--- SpringBoot-Proyecto02-LucaSteam/src/main/java/com/ejemplo/spring/controller/VideojuegoController.java
+++ SpringBoot-Proyecto02-LucaSteam/src/main/java/com/ejemplo/spring/controller/VideojuegoController.java
@@ -1,9 +1,12 @@
 package com.ejemplo.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplo.spring.service.VideojuegoService;
 
+@RestController
+@RequestMapping("/videojuegos")
 public class VideojuegoController {
 
 	@Autowired
