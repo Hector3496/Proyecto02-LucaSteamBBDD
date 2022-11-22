415053a08c40911de558af86e440d4272861c5e6
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-11-22 10:39:39 +0100

    Paquetes

--- SpringBoot-Proyecto02-LucaSteam/src/main/java/com/ejemplo/spring/controller/VideojuegoController.java
+++ SpringBoot-Proyecto02-LucaSteam/src/main/java/com/ejemplo/spring/controller/VideojuegoController.java
@@ -1,6 +1,7 @@
 package com.ejemplo.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplo.spring.service.VideojuegoService;
