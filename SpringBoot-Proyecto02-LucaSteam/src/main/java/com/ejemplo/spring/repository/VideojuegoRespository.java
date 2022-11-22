6c24b051f93498fbd039927ccb74af44bc3f89df
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-11-22 10:19:33 +0100

    Modificadas clases service y repository

--- SpringBoot-Proyecto02-LucaSteam/src/main/java/com/ejemplo/spring/repository/VideojuegoRespository.java
+++
@@ -1,9 +0,0 @@
-package com.ejemplo.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejemplo.spring.model.Videojuego;
-
-public interface VideojuegoRespository extends JpaRepository<Videojuego, Integer> {
-
-}
